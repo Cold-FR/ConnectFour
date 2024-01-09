d5ea119b4fa9fddc88cda9bcf34a0eb59fc3f085
davi0063 <clement.david1@etudiant.univ-reims.fr>
2024-01-09 08:55:20 +0100

    Fonction construirePion

--- Model/Pion.py
+++ Model/Pion.py
@@ -26,3 +26,17 @@ def type_pion(pion: dict) -> bool:
         and (pion[const.ID] is None or type(pion[const.ID]) == int)
 
 
+def construirePion(color: int) -> dict:
+    """
+    Fonction permettant de construire un pion
+    :param color: Couleur du pion à construire
+    :return: Dictionnaire représentant un pion
+    :raise TypeError: Si le paramètre n’est pas un entier
+    :raise ValueError: Si l’entier ne représente pas une couleur
+    """
+    if type(color) is not int:
+        raise TypeError('construirePion : Le paramètre n’est pas de type entier')
+    if color is not in const.COULEURS:
+        raise ValueError(f'construirePion : la couleur ({color}) n’est pas correcte')
+
+    return {const.COULEUR: color, const.ID: None}
