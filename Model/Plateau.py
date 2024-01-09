f80981e1ded11453866ee50804974a8daa1f770f
davi0063 <clement.david1@etudiant.univ-reims.fr>
2024-01-09 09:41:19 +0100

    Fonction placerPionPlateau : tests ok

--- Model/Plateau.py
+++ Model/Plateau.py
@@ -47,3 +47,33 @@ def construirePlateau() -> list:
             line.append(None)
         plateau.append(line)
     return plateau
+
+
+def placerPionPlateau(plateau: list, pion: dict, numCol: int) -> int:
+    """
+    Fonction plaçant un pion dans le plateau à la colonne correspondante
+    :param plateau: Tableau 2D représentant un plateau
+    :param pion: Dictionnaire représentant un pion
+    :param numCol: Entier représentant le numéro de la colonne
+    :return: Le numéro de la ligne où se retrouve le pion, ou -1 si la colonne est pleine
+    :raise TypeError: Si le premier paramètre n’est pas un plateau
+    :raise TypeError: Si le second paramètre n’est pas un pion
+    :raise TypeError: Si le troisième paramètre n’est pas un entier
+    :raise ValueError: Si le troisième paramètre n'est pas un numéro de colonne valide
+    """
+    if not type_plateau(plateau):
+        raise TypeError('placerPionPlateau : Le premier paramètre ne correspond pas à un plateau')
+    if not type_pion(pion):
+        raise TypeError('placerPionPlateau : Le second paramètre n\'est pas un pion')
+    if type(numCol) is not int:
+        raise TypeError('placerPionPlateau : Le troisième paramètre n\'est pas un entier')
+    if numCol < 0 or numCol > const.NB_COLUMNS - 1:
+        raise ValueError(f'placerPionPlateau : La valeur de la colonne ({numCol}) n\'est pas correcte')
+
+    i = -1
+    while i < const.NB_LINES - 1 and plateau[i+1][numCol] is None:
+        i += 1
+    if i != -1:
+        plateau[i][numCol] = pion
+
+    return i
