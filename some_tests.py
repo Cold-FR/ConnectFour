c08a9a0735d6f35cad2f3ed927e359a98321222b
davi0063 <clement.david1@etudiant.univ-reims.fr>
2024-01-10 17:28:37 +0100

    Tests fonction detecter4verticalPlateau

--- some_tests.py
+++ some_tests.py
@@ -16,9 +16,10 @@ print("\x1B[41m \x1B[0m : carré rouge ")
 print("\x1B[41mA\x1B[0m : A sur fond rouge")
 
 p = construirePlateau()
-for _ in range(20):
+for _ in range(30):
     placerPionPlateau(p, construirePion(choice(const.COULEURS)),
                       randint(0, const.NB_COLUMNS - 1))
 
 print(toStringPlateau(p))
 print(detecter4horizontalPlateau(p, const.JAUNE))
+print(detecter4verticalPlateau(p, const.JAUNE))
